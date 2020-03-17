58d391c9d2c109b08e1f84f18fdde0b879a077e9
vpslinuxinstall <shoparoundwyz@gmail.com>
2020-03-17 00:03:15 -0400

    commit

--- src/Assignment1/Copy.java
+++ src/Assignment1/Copy.java
@@ -11,11 +11,32 @@ public class Copy {
                 "Enter 0.0 when done!");
         double num;
         num = stdin.nextDouble();
+        // Your program will need to add 2 variables,
+        // say n and total (of types int and double, respectively)
+        // that keep a running count of the number of values
+        // read and the total up to that point.
+        // Initialize n and total
+        int n = 0;
+        double total = 0.0;
+        total = total + num;
+
         while (num != 0.0) {
             System.out.printf("%.2f\n", num);
             num = stdin.nextDouble();
+            // Both n and total start with zero initial values and
+            // have to be updated after each number is read.
+            // Increment n
+            n++;
+            // Add num to total
+            total = total + num;
 
         }
+        // Use the format items %d and %.2f to print integer (int) and
+        // floating-point numbers (double) values, respectively.
+        double average = total / n;
+        System.out.printf("The count of numbers is %d\n", n);
+        System.out.printf("The total numbers in the input is %.2f\n", total);
+        System.out.printf("The average is %.2f\n", average);
         System.out.println("Goodbye!");
         System.exit(0);
 
