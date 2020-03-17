3d3af0a438f06e71a66503e39a8cf8199b56c449
vpslinuxinstall <shoparoundwyz@gmail.com>
2020-03-17 13:07:16 -0400

    commit

---
+++ src/Assignment3/DatabaseLookup.java
@@ -0,0 +1,49 @@
+package Assignment3;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class DatabaseLookup {
+    public static void main(String[] args) throws FileNotFoundException {
+        // Read data file
+        // Read the number data in the Data3.txt file
+        var file = new File("/Users/YanzhiWang/IdeaProjects/JavaProgrammingTutorial/src/Assignment3/Data3.txt");
+        // Store number to arrayList
+        Scanner scannerFile = new Scanner(file);
+        var arrayList = new ArrayList<Integer>();
+        while (scannerFile.hasNext()) {
+            arrayList.add(scannerFile.nextInt());
+
+        }
+
+        // Read user input
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Number to Lookup: ");
+        while (scanner.hasNext()) {
+            // Read user input
+            int numberLookup = scanner.nextInt();
+
+            // Check if the data is in array
+            checkDataInArray(arrayList, numberLookup);
+
+            System.out.println("Number to Lookup: ");
+
+        }
+
+    }
+
+    static void checkDataInArray(ArrayList<Integer> array, int numberLookup) {
+        for (int numberFile : array) {
+            if (numberFile == numberLookup) {
+                System.out.println(numberLookup + " is in the database");
+                return;
+
+            }
+        }
+        System.out.println(numberLookup + " is not in the database");
+
+    }
+}
--- src/Assignment3/DirectoryLookupApplication.md
+++ src/Assignment3/DirectoryLookupApplication.md
@@ -1,2 +1,19 @@
 # Directory Lookup Application
 
+## What is the assignment
+
+Write a directory (database) lookup application which operates by
+
+1. reading a list of numbers stored in file Data3.txt to create a database of numbers:
+    - The list of numbers is preceded by a number (first number in the list) that specifies the size of the list. 
+    - This file name Data3.txt is supplied as a command-line argument to the application.
+2. accepting user input (supplied at the keyboard) which are numbers that it checks to see whether or not they are in its database.        
+
+For each number x input by the user the program displays (prints) “x is in the list” or “x is not in the list” depending upon whether or not x is in the list.
+
+The program terminates when the user enters Ctrl-Z (Ctrl-D) on the keyboard.
+
+
+
+
+
