34ceccc4b4fa42ed80c2244933914eab681d9f7d
vpslinuxinstall <shoparoundwyz@gmail.com>
2020-03-17 11:34:33 -0400

    commit

--- src/Assignment2/TemperatureConversion.java
+++ src/Assignment2/TemperatureConversion.java
@@ -4,24 +4,34 @@ import java.util.Scanner;
 
 public class TemperatureConversion {
     public static void main(String[] args) {
+        // Ask user to select the Temperature Converter
         System.out.println("Welcome to the Temperature Converter!");
         System.out.println("Enter 0 to convert F --> C and 1 to convert from C --> F");
         System.out.println("Enter Control-Z when done!");
         Scanner scanner = new Scanner(System.in);
         int selection = scanner.nextInt();
+        // Enter 0 to convert F --> C
         if (selection == 0) {
             System.out.println("Welcome to the F --> C Converter!");
             while (scanner.hasNext()) {
+                // Read user input
                 double f = scanner.nextDouble();
+                // Temperature conversion
                 double c = FToC(f);
+                // Print temperature
                 System.out.printf("%.02fF = %.02fC", f, c);
 
             }
-        } else {
+        }
+        // Enter 1 or greater number to convert from C --> F
+        else {
             System.out.println("Welcome to the C --> F Converter!");
             while (scanner.hasNext()) {
+                // Read user input
                 double c = scanner.nextDouble();
+                // Temperature conversion
                 double f = CTOF(c);
+                // Print temperature
                 System.out.printf("%.02fC = %.02fF", c, f);
 
             }
