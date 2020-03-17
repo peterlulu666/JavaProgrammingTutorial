5cec29c75a0d35586a90b5183153fe01875daf76
vpslinuxinstall <shoparoundwyz@gmail.com>
2020-03-17 13:09:22 -0400

    commit

--- src/Assignment3/DatabaseLookup.java
+++ src/Assignment3/DatabaseLookup.java
@@ -3,7 +3,6 @@ package Assignment3;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class DatabaseLookup {
